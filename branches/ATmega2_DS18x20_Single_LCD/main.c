8a569f69f86ce95400946084c3b6a276ef49f430
funlw65@gmail.com <funlw65@gmail.com@bf6e8a79-bcc5-4527-8921-19dd5fb2c3da>
2013-08-18 17:07:40 +0000

    ATmega2_DS18x20_Single_LCD project reads a single temperature sensor and displays on LCD the temperature value and the ID of the sensor. It also displays error messages and blink a LED when a bad event occurs.

---
+++ branches/ATmega2_DS18x20_Single_LCD/main.c
@@ -0,0 +1,149 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 18, 2013
+ *      Author: Vasile Guta Ciucur
+ *  Description:
+ *
+ *******************************************************************************
+ *  BSD License.
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  - Neither the name of Vasile Guta Ciucur nor the names of
+ *    its contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
+//Enable the following definitions on "atmegaclib2.h" header:
+
+//#define ENABLE_CONVERSION    // useful for Serial, LCD and 7SEG Display
+//#define ENABLE_ONE_WIRE    // one wire protocol
+//#define ENABLE_DS18_2_ // Dallas temperature sensors, require ONE_WIRE
+//#define ENABLE_LCD         // require CONVERSION
+
+//Scroll down the header file and, still in the user zone, search for and
+// change the following definitions, as bellow:
+
+//#define OW_ONE_BUS <-- uncomment this line.
+
+// search for the following lines and change to the required pin if you need.
+//#define OW_PIN  PC5
+//#define OW_IN   PINC
+//#define OW_DDR  DDRC
+//#define OW_OUT  PORTC
+
+// Search for the following and change them as follows:
+
+//#define OW_USE_INTERNAL_PULLUP    0  // 0=external, 1=internal
+
+// DS18x20 EERPROM support disabled(0) or enabled(1) :
+//#define DS18X20_EEPROMSUPPORT     0
+// decicelsius functions disabled(0) or enabled(1):
+//#define DS18X20_DECICELSIUS       1
+// max. resolution functions disabled(0) or enabled(1):
+//#define DS18X20_MAX_RESOLUTION    0
+// extended output via UART disabled(0) or enabled(1) :
+//#define DS18X20_VERBOSE           0 //require serial comm.
+
+// Also, check and modify (if required) the LCD pin and port definitions
+
+// Now, check if ATmegaCLib2 project has the same AVR target as your project,
+// and clean the ATmegaCLib2 project to delete the previous compiled code.
+// Now you are ready to compile and upload your project.
+
+
+
+#ifndef F_CPU
+#define F_CPU 16000000U // required by Atmel Studio 6
+#endif
+
+
+#include <avr/io.h>
+//#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <avr/pgmspace.h>
+#include <atmegaclib2.h>
+
+void stop(void){
+	for(;;){
+		onboard_led_toggle();
+		_delay_ms(250);
+	}
+}
+
+void main(void) __attribute__((noreturn));
+void main(void) {
+	uint8_t id[OW_ROMCODE_SIZE]; // our ROM_id
+	int16_t decicelsius;
+	int8_t temp_str[7];
+	uint8_t res, i;
+	//--
+	lcd_init();
+	onboard_led_enable();
+	onboard_led_off();
+	res = DS18X20_OK;
+	res = ow_rom_search(OW_SEARCH_FIRST, &id[0]);
+	if (res == OW_PRESENCE_ERR) {
+		//
+		lcd_set_cursor(0, 0);
+		lcd_puts_f("No Sensor found!");
+		stop();
+	}
+	if (res == OW_DATA_ERR) {
+		//
+		lcd_set_cursor(0, 0);
+		lcd_puts_f("Bus Error!      ");
+		stop();
+	}
+	while (1){
+		// start measurement for all sensors or a single(unique) sensor
+		res = DS18X20_start_meas(DS18X20_POWER_EXTERN, NULL);
+		if(res != DS18X20_OK){
+			//
+			lcd_set_cursor(0, 0);
+			lcd_puts_f( "Short Circuit!  ");
+			stop();
+		}
+		_delay_ms(DS18B20_TCONV_12BIT);
+		// read the temperature of the unique sensor
+		res=DS18X20_read_decicelsius_single(DS18B20_FAMILY_CODE, &decicelsius);
+		if(res != DS18X20_OK){
+			lcd_set_cursor(0, 0);
+			lcd_puts_f("Error reading T!");
+			stop();
+		}
+		DS18X20_format_from_decicelsius(decicelsius, temp_str, 7);
+		lcd_set_cursor(0, 0);
+		lcd_puts_f("T=         ");
+		lcd_set_cursor(2, 0);
+		lcd_puts(temp_str);
+		lcd_puts_f(" C");
+		// print the id code in hexadecimal
+		lcd_set_cursor(0, 1);
+		for(i = 0; i < OW_ROMCODE_SIZE; i++){
+			// showing the sensor id
+			lcd_puthexU08(id[i]);
+		}
+	}
+}
+
